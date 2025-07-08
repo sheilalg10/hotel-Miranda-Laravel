be66e32e5fd3cc7a008aa911eb868d3bfbbaf15e
sheilalg10 <sheilala6c@gmail.com>
2025-07-08 18:52:51 +0200

    add: rooms view

---
+++ resources/views/rooms.blade.php
@@ -0,0 +1,532 @@
+@extends('layouts.app')
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../styles/css/style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
+    <title>Hotel Miranda</title>
+  </head>
+  <body>
+    <!-- HEADER -->
+    <header class="header">
+      <span class="header__span">We Make Your Feel Comfortable</span>
+      <div class="header__div">
+        <div class="header__div__hamburguer" id="hamburguerMenu">
+          <span class="header__div__hamburguer__span"></span>
+          <span class="header__div__hamburguer__span"></span>
+          <span class="header__div__hamburguer__span"></span>
+        </div>
+        <a href="../index.html">
+          <img
+            class="header__div__logo"
+            src="../assets/images/title.svg"
+            alt="Hotel Miranda"
+          />
+        </a>
+        <nav class="header__div__nav">
+          <ul class="header__div__nav__list">
+            <li class="header__div__nav__list__item">
+              <a href="./aboutUs.html" class="header__div__nav__list__link"
+                >About Us</a
+              >
+            </li>
+            <li class="header__div__nav__list__item">
+              <a href="./rooms.html" class="header__div__nav__list__link"
+                >Rooms</a
+              >
+            </li>
+            <li class="header__div__nav__list__item">
+              <a href="./offers.html" class="header__div__nav__list__link"
+                >Offers</a
+              >
+            </li>
+            <li class="header__div__nav__list__item">
+              <a href="./contact.html" class="header__div__nav__list__link"
+                >Contact</a
+              >
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="header__icons">
+        <img
+          src="../assets/images/icons/human.svg"
+          class="header__icons__icon"
+          alt="User Icon"
+        />
+        <img
+          src="../assets/images/icons/lupa.svg"
+          class="header__icons__icon"
+          alt="Search Icon"
+        />
+      </div>
+    </header>
+    <!-- MAIN -->
+    <main>
+      <section class="title">
+        <span class="title__span">the ultimate luxury</span>
+        <h1 class="title__h1">ultimate room</h1>
+        <div class="title__div">
+          <p class="title__div__p">Home | <span>Rooms</span></p>
+        </div>
+      </section>
+      <section class="sectionrooms">
+        <div class="sectionrooms__item">
+          <img
+            class="sectionrooms__item__image"
+            src="../assets/images/room.webp"
+            alt="room"
+          />
+          <div class="rooms__icons sectionrooms__item">
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/bed.svg"
+              alt="bed"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/wifi.svg"
+              alt="wifi"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/car.svg"
+              alt="car"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cold.svg"
+              alt="cold"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/gym.svg"
+              alt="gym"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/noSmoking.svg"
+              alt="no smoking"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cocktail.svg"
+              alt="cocktail"
+            />
+          </div>
+          <h2 class="sectionrooms__item__title">minimal duplex room</h2>
+          <p class="sectionrooms__item__description">
+            Lorem ipsum dolor sit amet, consectetur adipi sicing elit, sed do
+            eiusmod tempor.
+          </p>
+          <div class="sectionrooms__item__actions">
+            <h3 class="sectionrooms__item__actions__price">
+              $345/Night<a href="./rooms/roomsDetails.html">Booking Now</a>
+            </h3>
+          </div>
+        </div>
+
+        <div class="sectionrooms__item">
+          <img
+            class="sectionrooms__item__image"
+            src="../assets/images/room02.jpg"
+            alt="room"
+          />
+          <div class="rooms__icons sectionrooms__item">
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/bed.svg"
+              alt="bed"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/wifi.svg"
+              alt="wifi"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/car.svg"
+              alt="car"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cold.svg"
+              alt="cold"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/gym.svg"
+              alt="gym"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/noSmoking.svg"
+              alt="no smoking"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cocktail.svg"
+              alt="cocktail"
+            />
+          </div>
+          <h2 class="sectionrooms__item__title">minimal duplex room</h2>
+          <p class="sectionrooms__item__description">
+            Lorem ipsum dolor sit amet, consectetur adipi sicing elit, sed do
+            eiusmod tempor.
+          </p>
+          <div class="sectionrooms__item__actions">
+            <h3 class="sectionrooms__item__actions__price">
+              $345/Night<a href="./rooms/roomsDetails.html">Booking Now</a>
+            </h3>
+          </div>
+        </div>
+
+        <div class="sectionrooms__item">
+          <img
+            class="sectionrooms__item__image"
+            src="../assets/images/room03.jpg"
+            alt="room"
+          />
+          <div class="rooms__icons sectionrooms__item">
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/bed.svg"
+              alt="bed"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/wifi.svg"
+              alt="wifi"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/car.svg"
+              alt="car"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cold.svg"
+              alt="cold"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/gym.svg"
+              alt="gym"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/noSmoking.svg"
+              alt="no smoking"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cocktail.svg"
+              alt="cocktail"
+            />
+          </div>
+          <h2 class="sectionrooms__item__title">minimal duplex room</h2>
+          <p class="sectionrooms__item__description">
+            Lorem ipsum dolor sit amet, consectetur adipi sicing elit, sed do
+            eiusmod tempor.
+          </p>
+          <div class="sectionrooms__item__actions">
+            <h3 class="sectionrooms__item__actions__price">
+              $345/Night<a href="./rooms/roomsDetails.html">Booking Now</a>
+            </h3>
+          </div>
+        </div>
+
+        <div class="sectionrooms__item">
+          <img
+            class="sectionrooms__item__image"
+            src="../assets/images/room04.jpg"
+            alt="room"
+          />
+          <div class="rooms__icons sectionrooms__item">
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/bed.svg"
+              alt="bed"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/wifi.svg"
+              alt="wifi"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/car.svg"
+              alt="car"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cold.svg"
+              alt="cold"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/gym.svg"
+              alt="gym"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/noSmoking.svg"
+              alt="no smoking"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cocktail.svg"
+              alt="cocktail"
+            />
+          </div>
+          <h2 class="sectionrooms__item__title">minimal duplex room</h2>
+          <p class="sectionrooms__item__description">
+            Lorem ipsum dolor sit amet, consectetur adipi sicing elit, sed do
+            eiusmod tempor.
+          </p>
+          <div class="sectionrooms__item__actions">
+            <h3 class="sectionrooms__item__actions__price">
+              $345/Night<a href="./rooms/roomsDetails.html">Booking Now</a>
+            </h3>
+          </div>
+        </div>
+
+        <div class="sectionrooms__item">
+          <img
+            class="sectionrooms__item__image"
+            src="../assets/images/room05.webp"
+            alt="room"
+          />
+          <div class="rooms__icons sectionrooms__item">
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/bed.svg"
+              alt="bed"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/wifi.svg"
+              alt="wifi"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/car.svg"
+              alt="car"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cold.svg"
+              alt="cold"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/gym.svg"
+              alt="gym"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/noSmoking.svg"
+              alt="no smoking"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cocktail.svg"
+              alt="cocktail"
+            />
+          </div>
+          <h2 class="sectionrooms__item__title">minimal duplex room</h2>
+          <p class="sectionrooms__item__description">
+            Lorem ipsum dolor sit amet, consectetur adipi sicing elit, sed do
+            eiusmod tempor.
+          </p>
+          <div class="sectionrooms__item__actions">
+            <h3 class="sectionrooms__item__actions__price">
+              $345/Night<a href="./rooms/roomsDetails.html">Booking Now</a>
+            </h3>
+          </div>
+        </div>
+
+        <div class="sectionrooms__item">
+          <img
+            class="sectionrooms__item__image"
+            src="../assets/images/room06.jpg"
+            alt="room"
+          />
+          <div class="rooms__icons sectionrooms__item">
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/bed.svg"
+              alt="bed"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/wifi.svg"
+              alt="wifi"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/car.svg"
+              alt="car"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cold.svg"
+              alt="cold"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/gym.svg"
+              alt="gym"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/noSmoking.svg"
+              alt="no smoking"
+            />
+            <img
+              class="rooms__icons__item"
+              src="../assets/images/icons/cocktail.svg"
+              alt="cocktail"
+            />
+          </div>
+          <h2 class="sectionrooms__item__title">minimal duplex room</h2>
+          <p class="sectionrooms__item__description">
+            Lorem ipsum dolor sit amet, consectetur adipi sicing elit, sed do
+            eiusmod tempor.
+          </p>
+          <div class="sectionrooms__item__actions">
+            <h3 class="sectionrooms__item__actions__price">
+              $345/Night<a href="./rooms/roomsDetails.html">Booking Now</a>
+            </h3>
+          </div>
+        </div>
+
+        <div class="pagination">
+          <img src="../assets/images/icons/pagArrowLeft.svg" alt="left arrow" />
+          <p class="pagination__numb active">1</p>
+          <p class="pagination__numb">2</p>
+          <p class="pagination__numb">3</p>
+          <p class="pagination__numb">...</p>
+          <p class="pagination__numb">10</p>
+          <img
+            src="../assets/images/icons/pagArrowRight.svg"
+            alt="right arrow"
+          />
+        </div>
+      </section>
+    </main>
+    <!-- FOOTER -->
+    <footer>
+      <div class="socials">
+        <img
+          class="socials__logo"
+          src="../assets/images/logoSolid.png"
+          alt="Hotel Miranda"
+        />
+        <p class="socials__description">
+          Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed doing
+          eius mod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitat ion ullamco laboris nisi.
+        </p>
+        <ul class="socials__list">
+          <li class="socials__list__item">
+            <img src="../assets/images/icons/facebook.svg" alt="facebook" />
+          </li>
+          <li class="socials__list__item socials__list__item-solid">
+            <img src="../assets/images/icons/twitter.svg" alt="twitter" />
+          </li>
+          <li class="socials__list__item">
+            <img src="../assets/images/icons/behance.svg" alt="behance" />
+          </li>
+          <li class="socials__list__item">
+            <img src="../assets/images/icons/linkedin.svg" alt="linkedin" />
+          </li>
+          <li class="socials__list__item">
+            <img src="../assets/images/icons/youtube.svg" alt="youtube" />
+          </li>
+        </ul>
+      </div>
+
+      <div class="services">
+        <h3 class="services__title">Services.</h3>
+        <ul class="services__list">
+          <div class="services__list__column">
+            <li class="services__list__column__item">resturent & bar</li>
+            <li class="services__list__column__item">swimming pool</li>
+            <li class="services__list__column__item">wellness & pa</li>
+            <li class="services__list__column__item">restaurant</li>
+            <li class="services__list__column__item">conference room</li>
+            <li class="services__list__column__item">coctail party house</li>
+          </div>
+          <div class="services__list__column">
+            <li class="services__list__column__item">gaming zone</li>
+            <li class="services__list__column__item">marrige party</li>
+            <li class="services__list__column__item">party planning</li>
+            <li class="services__list__column__item">tour consultancy</li>
+          </div>
+        </ul>
+      </div>
+
+      <div class="contact">
+        <h3 class="contact__title">Contact us</h3>
+
+        <div class="contact__section">
+          <img
+            class="contact__section__ico"
+            src="../assets/images/icons/phone.svg"
+            alt="phone"
+          />
+          <div class="contact__section__info">
+            <p class="contact__section__info__title">phone number</p>
+            <p class="contact__section__info__description">
+              +987 876 765 76 577
+            </p>
+          </div>
+        </div>
+
+        <div class="contact__section">
+          <img
+            class="contact__section__ico"
+            src="../assets/images/icons/mail.svg"
+            alt="mail"
+          />
+          <div class="contact__section__info">
+            <p class="contact__section__info__title">Email</p>
+            <p class="contact__section__info__description">
+              miranda-hotel@gmail.com
+            </p>
+          </div>
+        </div>
+
+        <div class="contact__section">
+          <img
+            class="contact__section__ico"
+            src="../assets/images/icons/map.svg"
+            alt="map"
+          />
+          <div class="contact__section__info">
+            <p class="contact__section__info__title">location</p>
+            <p class="contact__section__info__description">
+              calle san francisco 245, sevilla, españa
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="copyright">
+        <p>copyright Sheila Lara García - 2025</p>
+        <p>Terms of Use | Privacy Environmental Policy</p>
+      </div>
+    </footer>
+    <!-- JS -->
+    <script type="module" src="../js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  </body>
+</html>
+
